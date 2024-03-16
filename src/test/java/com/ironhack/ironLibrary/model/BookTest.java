ff2eba95df31446d936f8af722a92818c233e24c
JuliaORS <julia.olle.roset@gmail.com>
2024-03-16 15:52:27 +0100

    create Book class

---
+++ src/test/java/com/ironhack/ironLibrary/model/BookTest.java
@@ -0,0 +1,10 @@
+package com.ironhack.ironLibrary.model;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class BookTest {
+
+}
