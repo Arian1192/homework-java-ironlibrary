d27f47a780f83c86f8907a527afe4f2232a66442
Arian <arian.collaso.rodriguez@gmail.com>
2024-03-16 17:04:55 +0100

    remove contextloadtest

--- src/test/java/com/ironhack/ironLibrary/IronLibraryApplicationTests.java
+++ src/test/java/com/ironhack/ironLibrary/IronLibraryApplicationTests.java
@@ -3,11 +3,7 @@ package com.ironhack.ironLibrary;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class IronLibraryApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//@SpringBootTest
+//class IronLibraryApplicationTests {
+//
+//}
