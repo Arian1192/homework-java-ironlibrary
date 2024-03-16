73d1028953be42301df1f4ad47e0e78bd0b11fc2
victoriaschwin <vicky.schwint95@gmail.com>
2024-03-16 15:46:06 +0100

    author class defined

--- src/main/java/com/ironhack/ironLibrary/model/Author.java
+++ src/main/java/com/ironhack/ironLibrary/model/Author.java
@@ -1,7 +1,19 @@
 package com.ironhack.ironLibrary.model;
 
+import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Data;
+import lombok.Setter;
 
 @Data
+@Entity
+@Table(name = "authors")
 public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter(AccessLevel.NONE)
+    private Integer authorId;
+    private String name;
+    private String email;
+    private Book authorBook;
 }
