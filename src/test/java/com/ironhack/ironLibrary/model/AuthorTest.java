01da1a0a46bad704805bccac25259597e46c3dbf
victoriaschwin <vicky.schwint95@gmail.com>
2024-03-23 10:37:46 +0100

    create author repository & improve author testing

--- src/test/java/com/ironhack/ironLibrary/model/AuthorTest.java
+++ src/test/java/com/ironhack/ironLibrary/model/AuthorTest.java
@@ -2,6 +2,7 @@ package com.ironhack.ironLibrary.model;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class AuthorTest {
@@ -9,9 +10,12 @@ public class AuthorTest {
    @Test
     void createAuthor(){
        Author authorOne = new Author();
-       Author authorTwo = new Author();
+       Book book = new Book("978-3-16-148410-0", "Test book", "Tester", 2);
+       Author authorTwo = new Author("Carlos Diaz","carlosdiaz@mail.com", book);
 
        assertNotNull(authorOne);
        assertNotNull(authorTwo);
+       assertEquals("Carlos Diaz", authorTwo.getName());
+       assertEquals("Test book", authorTwo.getAuthorBook().getTitle());
    }
 }
