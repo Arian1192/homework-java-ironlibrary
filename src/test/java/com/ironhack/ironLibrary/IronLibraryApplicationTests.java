9cc4943285083014948037ed503421a9f7648897
Arian <arian.collaso.rodriguez@gmail.com>
2024-03-30 14:38:47 +0100

    Fix

--- src/test/java/com/ironhack/ironLibrary/IronLibraryApplicationTests.java
+++ src/test/java/com/ironhack/ironLibrary/IronLibraryApplicationTests.java
@@ -3,7 +3,7 @@ package com.ironhack.ironLibrary;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = IronLibraryApplication.class)
 class IronLibraryApplicationTests {
     @Test
     void contextLoads() {
