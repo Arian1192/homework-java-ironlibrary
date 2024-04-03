9325f051308b0ba88b768420497b501010a8c2c8
victoriaschwin <vicky.schwint95@gmail.com>
2024-04-03 19:30:55 +0200

    findAllBookJoinAuthor

--- src/main/java/com/ironhack/ironLibrary/repository/AuthorRepository.java
+++ src/main/java/com/ironhack/ironLibrary/repository/AuthorRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -13,6 +14,9 @@ public interface AuthorRepository extends JpaRepository<Author,Integer> {
 
     @Query("SELECT b From Author a JOIN a.authorBook b WHERE a.name = ?1")
     Optional<Book> findBookByAuthor(String authorName);
+
+    @Query("SELECT b.*, a.name, a.email FROM Author a INNER JOIN book b ON a.authorBook = b")
+    public List<Book>
 }
 
 // TO ASK
