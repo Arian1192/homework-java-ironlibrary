3b52fbafe29cd08e9799033f1c8b49fb66155127
victoriaschwin <vicky.schwint95@gmail.com>
2024-04-08 23:01:54 +0200

    new Student Model tests

--- src/test/java/com/ironhack/ironLibrary/model/StudentTest.java
+++ src/test/java/com/ironhack/ironLibrary/model/StudentTest.java
@@ -1,12 +1,18 @@
 package com.ironhack.ironLibrary.model;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class StudentTest {
-
-
+    private Student dummyStudent;
+    private Student dummyStudent2;
+    @BeforeEach
+    void setUp() {
+        dummyStudent = new Student("09003688800","Fran");
+        dummyStudent2 = new Student("09003688800","Fran");
+    }
     @Test
     void createStudent(){
         Student studentQA = new Student();
@@ -14,5 +20,28 @@ class StudentTest {
         assertNotNull(studentQA);
         assertNotNull(studentQA2);
     }
+
+    @Test
+    public void StudentSetterNameTest() {
+        dummyStudent.setName("Camila");
+        assertEquals("Camila", dummyStudent.getName());
+    }
+
+    @Test
+    public void StudentGetterNameTest() {
+        assertEquals("Fran", dummyStudent.getName());
+    }
+
+    @Test
+    void shouldBeCorrectInstantiated(){
+        assertEquals("09003688800", dummyStudent.getUsn());
+        assertEquals("Fran", dummyStudent.getName());
+    }
+
+    @Test
+    void shouldBeTheSameObject(){
+        assertEquals(dummyStudent, dummyStudent2);
+    }
+
 
 }
